7dc2e146665531e5a937d2fe8b2a9b80e5693734
Felix Briones <f.briones562@gmail.com>
2024-12-14 20:30:54 -0800

    Added xxd_re.rs. Currently only outputs the stdin

---
+++ xxd_re.rs
@@ -0,0 +1,23 @@
+// import std::io for stdin and stdout.
+// import self: import allows us to refer to the module itself
+// import BufRead: import allows us read from buffered source
+use std::io::{self, BufRead};
+
+fn main()
+{
+    // 'let' keyword allows us to declare an immuatable variable
+    let stdin = io::stdin();
+
+    // Create a locked version/handle of the standard input, preventing other parts of program 
+    // from using stdin at the same time possibly preventing race conditions.
+    // Locking stdin also lets us read larger chunks of data at once instead of char at a time
+    let handle = stdin.lock();
+
+    // Handles new line delimiters. Buffers input for efficient reading. Goes line by line
+    for line in handle.lines()
+    {
+        // unwrap extracts the string value if operation succeeds
+        let line = line.unwrap();
+        println!("Ouput: {}", line);
+    }
+}
